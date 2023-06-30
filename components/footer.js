f291acaa7495e4ec18ba70437ae5488e16af3001
maikelthedev <maikel@maikel.uk>
2023-06-30 12:09:18 +0200

    Modified the footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,10 @@
 function Footer() {
-  return <div>Whatever on the footer</div>;
+  /* I need using TAilwind CSS in combination with NextJS a return JSX code that creates a footer saying something like "Maikel OÜ Copyright XXX" being the XXX the current year. */
+  return (
+    <div className="bg-gray-800 text-white text-center p-4">
+      <p>Maikel OÜ © 2020 - {new Date().getFullYear()}</p>
+    </div>
+  );
 }
 
 export default Footer;
--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,6 @@
 import Image from "next/image";
 
 export default function NotFound() {
-  // Using Tailwind CSS return a centered image of culkin.jpg with the text "404 - Page Not Found" and the image inside a circle frame.
-  // The image should be 300px wide and 300px tall. The text should be 2rem in size and bold.
-  // Separate slightly the text from the image and make only the 404 bold, the rest of the text should be normal.
-  // The text should be centered horizontally and vertically.
-  // The image should be centered horizontally and vertically.
   return (
     <div className="flex flex-col items-center justify-center py-8">
       <div className="flex flex-col items-center justify-center flex-1 px-20 text-center">
