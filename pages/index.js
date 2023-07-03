93d3bd84a52b723c1043acafd250ada60225c925
maikelthedev <maikel@maikel.uk>
2023-07-03 10:37:47 +0200

    not much changed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
-
+import Image from "next/image";
 
 import {
   ArrowPathIcon,
@@ -8,9 +8,7 @@ import {
   FingerPrintIcon,
 } from "@heroicons/react/24/outline";
 
-import {
-  LockClosedIcon,
-} from "@heroicons/react/20/solid";
+import { LockClosedIcon } from "@heroicons/react/20/solid";
 const features = [
   {
     name: "Bollocks 1",
@@ -38,30 +36,39 @@ const features = [
   },
 ];
 
-
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Home() {
-
   return (
     <div>
       <div className="bg-white py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:text-center">
-            <h2 className="text-base font-semibold leading-7 text-indigo-600">
-              Tiny bollocks
-            </h2>
-            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              Big long sentence of bollocks
-            </p>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
-              Suspendisse eget egestas a elementum pulvinar et feugiat blandit
-              at. In mi viverra elit nunc.
-            </p>
-          </div>
+            <div className="flex h-screen h-full justify-center items-center mb-3">
+              <Image
+                className="text-center"
+                src="/nowords.png"
+                alt="Picture of the author"
+                width={200}
+                height={200}
+              />
+            </div>
+            
+              <h2 className="text-base font-semibold leading-7 text-indigo-600">
+                Tiny bollocks
+              </h2>
+              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                Big long sentence of bollocks
+              </p>
+              <p className="mt-6 text-lg leading-8 text-gray-600">
+                Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
+                Suspendisse eget egestas a elementum pulvinar et feugiat blandit
+                at. In mi viverra elit nunc.
+              </p>
+            </div>
+          
           <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
             <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
               {features.map((feature) => (
