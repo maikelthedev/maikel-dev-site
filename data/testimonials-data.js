72c3c72c9360a2ba1291bf2aac876f8307247efe
maikelthedev <maikel@maikel.uk>
2023-07-06 23:36:31 +0200

    Testimonials without bottom padding

---
+++ data/testimonials-data.js
@@ -0,0 +1,34 @@
+export const testimonials = [
+  {
+    id: 1,
+    name: "Dave Cottlehuber",
+    role: "CEO @ SkunkWerks",
+    content:
+      "I think you did an incredible job of landing on all four feet (like a cat!) in quite unfamiliar terrain, picking up OCI like you used it already, and navigating a lot of incomplete information on FreeBSD as well.",
+    logo: "/clients/skunkwerks.webp",
+  },
+  {
+    id: 2,
+    name: "John Gibb",
+    role: "Head of Projects @ Survation",
+    logo: "/clients/surva.webp",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget arcu eu ipsum sodales euismod.",
+  },
+  {
+    id: 2,
+    name: "Mike Burrows",
+    role: "Former CTO @ Survation",
+    logo: "/clients/surva.webp",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget arcu eu ipsum sodales euismod.",
+  },
+  {
+    id: 2,
+    name: "Rushaa Hamid",
+    role: "Former Special Projects Manager @ Survation",
+    logo: "/clients/surva.webp",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget arcu eu ipsum sodales euismod.",
+  },
+];
--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import Hero from "@/components/hero";
+import { testimonials } from "@/data/testimonials-data";
 
 function Testimonials() {
   const header = {
@@ -8,45 +9,11 @@ function Testimonials() {
     slogan:
       "I have collaborated with many people over the years, and here are some of their thoughts about me",
   };
-  const testimonials = [
-    {
-      id: 1,
-      name: "Dave Cottlehuber",
-      role: "CEO @ SkunkWerks",
-      content:
-        "I think you did an incredible job of landing on all four feet (like a cat!) in quite unfamiliar terrain, picking up OCI like you used it already, and navigating a lot of incomplete information on FreeBSD as well.",
-      logo: "/clients/skunkwerks.webp",
-    },
-    {
-      id: 2,
-      name: "John Gibb",
-      role: "Head of Projects @ Survation",
-      logo: "/clients/surva.webp",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget arcu eu ipsum sodales euismod.",
-    },
-    {
-      id: 2,
-      name: "Mike Burrows",
-      role: "Former CTO @ Survation",
-      logo: "/clients/surva.webp",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget arcu eu ipsum sodales euismod.",
-    },
-    {
-      id: 2,
-      name: "Rushaa Hamid",
-      role: "Former Special Projects Manager @ Survation",
-      logo: "/clients/surva.webp",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget arcu eu ipsum sodales euismod.",
-    },
-  ];
 
   return (
     <Fragment>
       <Hero header={header} />
-      <div className="bg-gray-100 min-h-screen p-8">
+      <div className="bg-gray-100 p-8">
         <div className="max-w-3xl mx-auto">
           <h1 className="text-3xl font-bold mb-8">Testimonials</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
