cbd534d8921f40fa530678098b1645afde70f886
maikelthedev <maikel@maikel.uk>
2023-07-07 19:19:17 +0200

    Masonry layout to testimonials

--- data/testimonials-data.js
+++ data/testimonials-data.js
@@ -5,17 +5,8 @@ export const testimonials = [
     role: "CEO @ SkunkWerks",
     content:
       "I think you did an incredible job of landing on all four feet (like a cat!) in quite unfamiliar terrain, picking up OCI like you used it already, and navigating a lot of incomplete information on FreeBSD as well.",
-      link: "https://www.linkedin.com/in/davecottlehuber/",
+    link: "https://www.linkedin.com/in/davecottlehuber/",
     logo: "/clients/skunkwerks.webp",
-  },
-  {
-    id: 2,
-    name: "John Gibb",
-    role: "Head of Projects @ Survation",
-    logo: "/clients/surva.webp",
-    link: "https://www.linkedin.com/in/john-gibb-86b705a9/",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget arcu eu ipsum sodales euismod.",
   },
   {
     id: 2,
@@ -27,7 +18,25 @@ export const testimonials = [
       "Maikel designs systems to solve complete business problems, rapidly learning whatever is needed to detect, resolve and prevent issues for the long term. His effort and commitment in the workplace is always impressive and I recommend his services to anyone needing a rapid build-out of a stand-alone project or resolution to technical issues with existing deployments.",
   },
   {
-    id: 2,
+    id: 3,
+    name: "John Gibb",
+    role: "Head of Projects @ Survation",
+    logo: "/clients/surva.webp",
+    link: "https://www.linkedin.com/in/john-gibb-86b705a9/",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget arcu eu ipsum sodales euismod.",
+  },
+  {
+    id: 4,
+    name: "Hina Hussain/",
+    role: "Office and Operations Manager @ Survation",
+    logo: "/clients/surva.webp",
+    link: "https://www.linkedin.com/in/hina-hussain/",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget arcu eu ipsum sodales euismod.",
+  },
+  {
+    id: 5,
     name: "Rushaa Hamid",
     role: "Former Special Projects Manager @ Survation",
     logo: "/clients/surva.webp",
--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -3,6 +3,7 @@ import Hero from "@/components/hero";
 import { testimonials } from "@/data/testimonials-data";
 import { headers } from "@/data/headers";
 import Link from "next/link";
+
 function Testimonials() {
   const header = headers.testimonials;
 
@@ -12,24 +13,25 @@ function Testimonials() {
       <div className="bg-gray-100 p-8">
         <div className="max-w-3xl mx-auto">
           <h1 className="text-3xl font-bold mb-8">Testimonials</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6"> 
+          <div className="columns-2">
             {testimonials.map((testimonial) => (
-              <div
-                key={testimonial.id}
-                className="relative bg-white p-6 rounded shadow"
-              >
-                <p className="text-gray-800 mb-2">{testimonial.content}</p>
-                <Link href={testimonial.link}>
-                  <p className="text-gray-600 font-medium hover:underline ">
-                    {testimonial.name}
-                  </p>
-                </Link>
-                <p className="text-gray-500 text-sm">{testimonial.role}</p>
-                <img
-                  src={testimonial.logo}
-                  alt="Company Logo"
-                  className="absolute bottom-2 right-2 h-8 w-8"
-                />
+              <div key={testimonial.id} className="w-full py-2">
+                <div className="bg-white rounded shadow relative">
+                  <div className="p-6">
+                    <p className="text-gray-800 mb-2">{testimonial.content}</p>
+                    <Link href={testimonial.link}>
+                      <p className="text-gray-600 font-medium hover:underline">
+                        {testimonial.name}
+                      </p>
+                    </Link>
+                    <p className="text-gray-500 text-sm">{testimonial.role}</p>
+                  </div>
+                  <img
+                    src={testimonial.logo}
+                    alt="Company Logo"
+                    className="absolute bottom-2 right-2 h-8 w-8"
+                  />
+                </div>
               </div>
             ))}
           </div>
