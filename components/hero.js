8a81a6900932a85bf450778149dc48352c4c2466
maikelthedev <maikel@maikel.uk>
2023-07-12 14:28:50 +0200

    Made the slogan change on every page

--- components/hero.js
+++ components/hero.js
@@ -1,8 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 function Hero(props) {
   const { subtitle, title, slogan } = props.header;
+  const [currentTitle, setCurrentTitle] = useState(title[0]);
+  const [currentSubtitle, setCurrentSubtitle] = useState(subtitle[0]);
+
+  useEffect(() => {
+    let titleIndex = 0;
+    let subtitleIndex = 0;
+
+    const mainTitleInterval = setInterval(() => {
+      titleIndex = (titleIndex + 1) % title.length;
+      setCurrentTitle(title[titleIndex]);
+    }, 10000); // Change the main slogan every 10 seconds
+
+    const subtitleInterval = setInterval(() => {
+      subtitleIndex = (subtitleIndex + 1) % subtitle.length;
+      setCurrentSubtitle(subtitle[subtitleIndex]);
+    }, 3000); // Change the subtitle every 3 seconds
+
+    return () => {
+      clearInterval(mainTitleInterval);
+      clearInterval(subtitleInterval);
+    };
+  }, []);
+
+
   return (
     <div className="custom-bg-gray-100   md:py-10">
       <div className="mx-auto p-3 md:p-0 md:px-8">
@@ -11,7 +36,7 @@ function Hero(props) {
         shadow-slate-700 md:shadow-md"
         >
           <div className="group grid justify-items-center items-center mb-3 md:min-w-fit md:mr-10">
-            <Link href="/contact">
+            <Link href="/hire">
               <Image
                 className="md:group-hover:scale-90 group-hover:shadow-none duration-200 shadow-lg mb-3 shadow-slate-700 rounded-full border-dashed  md:group-hover:mb-0"
                 src="/me.webp"
@@ -28,10 +53,10 @@ function Hero(props) {
           </div>
           <div className="flex flex-col justify-center items-center">
             <h2 className="font-semibold leading-7 dark:text-indigo-400 text-indigo-600">
-              {subtitle}
+              {currentSubtitle}
             </h2>
             <p className="mt-2 text-3xl font-bold tracking-tight dark:text-white text-black md:text-4xl text-center">
-              {title}
+              {currentTitle}
             </p>
             <p className="mt-6 text-lg leading-8 dark:text-gray-400 text-gray-600">{slogan}</p>
           </div>
