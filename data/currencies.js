4ad2392e6256d57d6a1093c03652bf3a74fb96db
maikelthedev <maikel@maikel.uk>
2023-07-26 17:32:57 +0200

    WIP

---
+++ data/currencies.js
@@ -0,0 +1,33 @@
+export const currencies = [
+    {
+      denomination: "USD",
+      symbol: "$",
+      link: "https://donate.stripe.com/fZedTZ3Y47vjfyUaEE",
+    },
+    {
+      denomination: "EUR",
+      symbol: "€",
+      link: "https://donate.stripe.com/eVa03966ceXLfyU9AB",
+    },
+    {
+      denomination: "GBP",
+      symbol: "£",
+      link: "https://donate.stripe.com/dR69DJ3Y4dTHbiE146",
+    },
+    {
+      denomination: "AUD",
+      symbol: "$",
+      link: "https://donate.stripe.com/4gw6rx8ek3f3gCY5kn",
+    },
+    {
+      denomination: "JPY",
+      symbol: "¥",
+      link: "https://donate.stripe.com/14k7vB9iog1PcmI6os",
+    },
+    {
+      denomination: "AMD",
+      symbol: "֏",
+      link: "https://donate.stripe.com/cN22bh3Y4aHvfyU005",
+    },
+  ];
+  
