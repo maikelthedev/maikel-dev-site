d810a5be547480c950bc434892b0b1077dd0a66c
maikelthedev <maikel@maikel.uk>
2023-07-31 22:22:01 +0200

    WIP

--- pages/preview.js
+++ pages/preview.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import Link from "next/link";
 
 export default function Preview() {
   return (
@@ -7,7 +8,7 @@ export default function Preview() {
         <div className="custom-text-black flex flex-col md:flex-row p-5 custom-bg-white md:mx-auto md:max-w-6xl items-center">
           <div className="px-8 py-4 space-y-6">
             <h1 className="text-3xl font-bold">
-              This is a preview of a payment page
+              This is a preview of a concept of a payment page
             </h1>
             <p className="text-lg">
               Don't go through these payments because they are active. I'm using
@@ -22,6 +23,9 @@ export default function Preview() {
             <div className="px-8 py-4 space-y-6">
               <h1 className="text-3xl font-bold">Packages</h1>
               <p className="text-lg">This of text bit is still outside Stripe control</p>
+              <p className="text-lg">
+                If you were already a client you could control all of this through <Link className="font-bold hover:text-red-600" href="https://billing.stripe.com/p/login/8wM4i3fawaEE7Je9AA" target="_blank">their customer portal. </Link>
+              </p>
             </div>
           </div>
           <div className="p-10">
