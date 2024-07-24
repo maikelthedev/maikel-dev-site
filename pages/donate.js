77ab39f1460ad4283ba705171f998e09bb93440d
Maikel <github@maikel.dev>
2024-07-24 21:27:13 +0200

    Baseline donate page

--- pages/donate.js
+++ pages/donate.js
@@ -85,46 +85,14 @@ export default function Donations() {
         />
       </Head>
       <div className="custom-bg-white">
-        <Hero header={header} />
 
         <div className="custom-text-black flex flex-col md:flex-row p-2 custom-bg-white md:mx-auto md:max-w-6xl items-center">
           <div className="px-2 md:px-8 py-4 space-y-6">
-            <h1 className="text-3xl font-bold">I'm on a very tight budged 😅 </h1>
+            <h1 className="text-3xl font-bold">Donation Links </h1>
             <p className="text-lg">
-              As you might or might not know I'm running on a very tight budget to get rid of all the credit card debt. 
-              That means every month I spend the least possible to keep my sanity while paying as much as possible on credit card debt. 
-              There's still a few days to payday and I run out of funds so this is about a bit of a stretch to get there slightly more comfortably. 
-              If you have anyone on the fediverse with more urgent needs PLEASE DO consider donating to them instead. 
+            This page has all the donations link available for Maikel a.k.a.: Miguel Frias Mosquea or in the fediverse as <Link href="https://vmst.io/@maikel">@maikel@vmst.io</Link>
             </p>
-            <p className="text-lg text-red-700 font-bold">
-            </p>
-            <p className="text-lg">
-              Do not look for a sobbing story, this is just a matter of being stretched to thin with debt repayment. 
-            </p>
-            <h1 className="text-3xl font-bold">What is the goal?</h1>
-            <p>I think with ~300 extra euros I can get over the line. Anything above that will go towards reducing my debt pressure.</p>
-            <p>IMPORTANT INFO: Instantaneous are PayPal, Monzo, Revolut, Bizum</p>
-            <p>With <span className="text-3xl">PAYPAL</span> the funds reach me instantaneously</p>
-            <p>Kofi or Stripe take at least 3 days to show.</p>
-            {/*<div className="flex justify-between mb-1">
-              <span className="text-base font-medium text-blue-700 dark:text-white">
-                Donations Raised
-              </span>
-              <span className="text-sm font-medium text-blue-700 dark:text-white">
-                {totalEuros}€ of {neededAmount}€
-              </span>
-            </div>
-            <div className="w-full bg-gray-400 rounded-full dark:bg-gray-700">
-              <div
-                className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
-                style={{ width: currentPercentageString }}
-              >
-                {" "}
-                {currentPercentage.toFixed(2)}%
-              </div>
-            </div>
-  */}
-            <p>Updated: Friday 19th of April of 2024</p>
+
             <div className="">
               <table className="w-full flex flex-row flex-nowrap sm:bg-white rounded-lg  sm:shadow-lg my-5">
                 <thead className="custom-text-black">
@@ -177,26 +145,7 @@ export default function Donations() {
               Donate now by clicking on your currency symbol
             </h1>
             <Currencies />
-            <h1 className="text-3xl font-bold">How's your donation used:</h1>
-
-            <p className="text-lg">
-              Any funds go towards food and other expenses all the way until payday. If anything is spared past that, it'll go towards debt repayment. If I am in the black by next month, anything above it goes to the Moshidon project.  
-              Right now my debts are:
-            </p>
-              <ol>
-                <li>Virgin Money: 1787.03 GBP</li>
-                <li>Monzo Flex:  234.98 GBP </li>
-                <li>Zable credit card: 272.25 GBP</li>
-              </ol>
-            <p>Of the al Virgin is the urgent one because that's the one with the highest interest, the others are month rolling so 0% so I'm keeping Virgin not higher than 2000€ (my monthly salary which converst to roughly 1700 GBP) so on next payday I can pay it in full and reset the APR to 0%. But that also means I'm quickly running out of funds in the  other two, which have a much lower allowance.</p>
-            <h1 className="text-3xl font-bold" id="thankYou">
-              Thank you{" "}
-            </h1>
-            <p className="text-lg">
-              To all those who have donated. For safety and privacy reasons I'm
-              not going to disclose your names but for transparency and
-              accountability here is the list.
-            </p>
+        
             {
             // <ul className="list-disc px-8 space-y-2">
             //   {donations.map((donation) => (
